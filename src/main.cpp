41beb681cd5f6a077900ea07534528e223a04d9c
roshea6 <rpo1202@gmail.com>
2021-03-13 21:26:50 +0000

    Tuned PID values further

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main() {
    */
   
   // Initialize the PID controller with the coefficients
-  pid.Init(0.12, 0.003, 0.8);
+  pid.Init(0.11, 0.0004, 0.6);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
